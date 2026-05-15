adfc71ff50b1ffc9e1189aaf69c0edbbc7f9afb2
NeoByte <github@thatneobyte.com>
2026-05-15 16:20:56 +0200

    Added reply button (#11)
    * Added reply button
    
    * minor style adjustment
    
    ---------
    
    Co-authored-by: cmd430 <cmd430@gmail.com>

--- client/components/ChatInput.vue
+++ client/components/ChatInput.vue
@@ -228,6 +228,25 @@ export default defineComponent({
 			uploadInput.value?.click();
 		};
 
+		const onReplyMessage = (replyText: string) => {
+			const nextMessage = `${props.channel.pendingMessage}${replyText}`;
+
+			props.channel.pendingMessage = nextMessage;
+			props.channel.inputHistoryPosition = 0;
+
+			if (!input.value) {
+				return;
+			}
+
+			input.value.click();
+			input.value.focus();
+			input.value.value = nextMessage;
+			setInputSize();
+
+			const cursorPosition = nextMessage.length;
+			input.value.setSelectionRange(cursorPosition, cursorPosition);
+		};
+
 		const blurInput = () => {
 			input.value?.blur();
 		};
@@ -264,6 +283,7 @@ export default defineComponent({
 
 		onMounted(() => {
 			eventbus.on("escapekey", blurInput);
+			eventbus.on("message:reply", onReplyMessage);
 
 			if (store.state.settings.autocomplete) {
 				if (!input.value) {
@@ -376,6 +396,7 @@ export default defineComponent({
 
 		onUnmounted(() => {
 			eventbus.off("escapekey", blurInput);
+			eventbus.off("message:reply", onReplyMessage);
 
 			if (autocompletionRef.value) {
 				autocompletionRef.value.destroy();
